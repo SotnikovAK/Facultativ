d0b889611a118581af38147b1dc7acdaee640926
SotnikovAK <91308111+SotnikovAK@users.noreply.github.com>
2022-10-17 09:14:03 +0300

    Delete DOP directory

--- DOP/numpy_assistance.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-_array = np.loadtxt('3.txt', skiprows=1)
-t,v,x = _array[:,0],_array[:,2],_array[:,1]
-
-plt.figure(1)
-plt.plot(t, v * v + x * x - 1)
-plt.show()
